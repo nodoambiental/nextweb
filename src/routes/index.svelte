7fb66860a416b261c1aa064600d50be641e36188
Ágata Ordano <ordanoagata@gmail.com>
2021-12-23 16:50:11 -0300

    Boilerplate initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<span class="countdown">
+    <span style="--value: 60;"></span>
+  </span>
+  <button class="btn">neutral</button> 
+<button class="btn btn-primary">primary</button> 
+<button class="btn btn-secondary">secondary</button> 
+<button class="btn btn-accent">accent</button> 
+<button class="btn btn-ghost">ghost</button> 
+<button class="btn btn-link">link</button>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui')]
+};
+
+module.exports = config;
