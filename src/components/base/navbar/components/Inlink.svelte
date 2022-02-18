9c35f0fea5d5ce5fc268ae5e59424856b88635f4
Ágata Ordano <ordanoagata@gmail.com>
2022-02-18 20:57:42 -0300

    feat(navbar): add some base components to include in the future

---
+++ src/components/base/navbar/components/Inlink.svelte
@@ -0,0 +1,6 @@
+<script lang='ts'>
+    import type * as Types from '@components/model';
+	import Icon from '@iconify/svelte/dist/offline';
+    import * as Classes from '@components/style'
+</script>
+
